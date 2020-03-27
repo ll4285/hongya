c2290e8bdbeeac7a7d905e1f79adafa68b8df583
liulei <ll4285@126.com>
2020-03-28 00:40:21 +0800

    master:引入RabbitMq消息队列

--- src/test/java/com/ll/demo/DemoApplicationTests.java
+++ src/test/java/com/ll/demo/DemoApplicationTests.java
@@ -2,15 +2,36 @@ package com.ll.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
-    @Test
+    @Autowired
+    RabbitTemplate rabbitTemplate ;
+
+/*    @Test
     public void contextLoads() {
+        Map<String,Object> map = new HashMap<>();
+        map.put("keys","这是第一个消息！") ;
+        map.put("data", Arrays.asList("helloword",123,true)) ;
+        rabbitTemplate.convertAndSend("exchange.direct","atguigu.news",map);
     }
+
+    @Test
+    public void test1(){
+        Object o = rabbitTemplate.receiveAndConvert("atguigu.news");
+        System.out.println(o.getClass());
+        System.out.println(o);
+    }*/
+
 
 }
